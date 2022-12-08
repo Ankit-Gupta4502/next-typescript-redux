79b7d23cac00002359b44aba72a39f9ccb0db0bd
Ankit-Gupta4502 <83642182+Ankit-Gupta4502@users.noreply.github.com>
2022-12-08 10:26:50 +0530

    update action

--- redux/Actions.tsx
+++ redux/Actions.tsx
@@ -2,7 +2,7 @@
 export const getTodos =()=>async(dispatch:any)=>{
     try {
         dispatch({type:"GET_TODOS",payload:{title:"TEST"}})
-    } catch (error) {
-        
+    } catch (error:any) {
+        dispatch({type:"GET_TODOS",payload:error.response.data})
     }
 }
